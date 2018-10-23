e3b020a1e8deab438956d07442fb81d51f337d4f
OblakZan <jstsmjst0101@gmail.com>
2018-10-23 10:23:50 +0200

    popravki
    neki čudni simboli so bli

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 
 <?php include "header.html"; ?>
     <body>
-<<<<<<< HEAD
         <div>Novo</div>
-=======
         <div>TODO write content. Dodajanje Contenta v prvi DIV</div>
->>>>>>> 4cb0cd2417e49eaada69e5c46eedfd76724fc089
 <?php include "footer.html"; ?>
