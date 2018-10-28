0e469a17dd104f5374a3d734bf710c60fbabf500
OblakZan <jstsmjst0101@gmail.com>
2018-10-28 10:16:34 +0100

    header update
    Header sm že mal oblikovau tolk da ma nek basic shape drugo bo tk Blaz naredu haha

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 
 <?php include "header.html"; ?>
-    <body>
-        <div>Novo</div>
-        <div>TODO write content. Dodajanje Contenta v prvi DIV</div>
+<div style="border:1px solid black; font-size: 26pt; text-align: center;"> Najbolj popularno!</div>
+<?php
+/* pobrati podatke iz baze ter jih izpisati tukaj v div.
+ * vsak izdelek v svojem divu, div poleg diva, 3-4 izdelki v vrsti MAX
+ * začnemo z najpopularnejšimi nato vedno manj. 
+ */
+?>
 <?php include "footer.html"; ?>
