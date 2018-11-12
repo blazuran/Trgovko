5126325d5e6cc61496fa8f769fac7ecfc5d2c399
Ivan Tomić <ivan.tomic17@gmail.com>
2018-11-12 19:41:06 +0100

    priljublje slike popravljeno

--- favorites.php
+++ favorites.php
@@ -5,18 +5,20 @@ include_once "db.php";
 <?php 
 
 $UserID = $_SESSION['ID'];
-$sql = "SELECT * FROM products INNER JOIN favorites ON products.ID = favorites.Products_ID WHERE Users_ID='$UserID' ORDER BY Rating DESC";
+$sql = "SELECT f.ID as favoriteID, p.ID as productID, p.ProductURL as pProductURL, p.Title as pTitle, p.Price as pPrice "
+        . "FROM products as p INNER JOIN favorites f ON p.ID = f.Products_ID WHERE f.Users_ID='$UserID' ORDER BY Rating DESC";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // nedela za slike na samo na favorites
     while($row = $result->fetch_assoc()) {
-        $id = $row["ID"];
-        $sqlPicture = "SELECT * FROM Pictures WHERE Products_ID='$id' LIMIT 1";
+        $id = $row["favoriteID"];
+        $idproduct = $row["productID"];
+        $sqlPicture = "SELECT * FROM Pictures WHERE Products_ID='$idproduct' LIMIT 1";
         $resultPicture = $conn->query($sqlPicture);
         $rowPicture = $resultPicture->fetch_assoc();
         
-        echo "<br><a href=". $row["ProductURL"] .">". $row["Title"]. "</a>  Cena:" . $row["Price"] . "<a href='deletefavorite.php?id=$id'> Odstrani iz priljubljenih.</a><img src=". $rowPicture["url"] ." alt=". $rowPicture["Title"] ." height='60' width='100'>";
+        echo "<br><a href=". $row["pProductURL"] .">". $row["pTitle"]. "</a>  Cena:" . $row["pPrice"] . 
+                "<a href='deletefavorite.php?id=$id'> Odstrani iz priljubljenih.</a><img src=". $rowPicture["url"] ." alt=". $rowPicture["Title"] ." height='60' width='100'>";
        
     }
 } else {
