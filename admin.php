c1002ec1d4858191aae5f20a3b5732879dbd6f38
Ivan Tomić <ivan.tomic17@gmail.com>
2018-12-09 17:56:00 +0100

    admin

--- admin.php
+++ admin.php
@@ -20,19 +20,19 @@ echo "<table border='1'>
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<form action='edit.php?edit=<?= ".$row['ID']. "?>' method='POST'>";
+        echo "<form action='editUsers.php?id=".$row['ID']. "' method='POST'>";
         echo "<tr>";
         echo "<td><input type='text' name='Name' value='" . $row['Name'] . "' /><br></td>";
         echo "<td><input type='text' name='LastName' value='" . $row['LastName'] . "' /><br></td>";
         echo "<td><input type='text' name='Email' value='" . $row['Email'] . "' /><br></td>";
         if($row['Admin']==1){
-       echo "<td><input type='checkbox' name='Admin' value='true' checked='checked'/><br></td>";
+       echo "<td><input type='checkbox' name='Admin' value='1' checked='checked'/><br></td>";
             }
         else{
-       echo "<td><input type='checkbox' name='Admin' value='false'/><br></td>";
+       echo "<td><input type='checkbox' name='Admin' value='0'/><br></td>";
             }
-        echo "<td><input type='submit' value='Spremeni' /></td>";
-        echo "<td><a href='deleterow.php?id='".$row['ID']."'>Odstrani</a></td>";
+        echo "<td><input type='submit' name='Spremeni'  value='Spremeni' /></td>";
+        echo "<td><input type='submit' name='Odstrani' value='Odstrani'></td>";
         echo "</tr>";
         echo "</form>";
     }
