5fb9e6142e6c5a0dfed0d71e069fc9edb826ccfe
OblakZan <jstsmjst0101@gmail.com>
2019-01-07 14:34:52 +0100

    Slike
    Izpis default slike pri Results.php in Index.php v primeru da ni slike v bazi

--- results.php
+++ results.php
@@ -72,7 +72,6 @@ $result = $conn->query($sql);
     <form class="clear" action="orderByResults.php?">
         <input type="hidden" name="Search" value="<?php echo $search_value; ?>"/>
         <select name="order" onchange="this.form.submit()">
-            <option value="Rating DESC">Ocena</option>
             <option value="Price ASC">Cena (najnižja najprej)</option>
             <option value="Price DESC">Cena (najvišja najprej)</option>
             <option value="Title ASC">Naziv izdelka (A-Z)</option>
@@ -104,8 +103,8 @@ $result = $conn->query($sql);
                     <div class="block inspace-30 borderedbox">
                         <h6 class="font-x1">
                             <?php
-                            echo "Name: <a href='product.php?id=$id'>".$row["Title"]."</a><br>if($rowPicture[url]=empty(){<img src=" . $rowPicture["url"] . " alt=" . $rowPicture["Title"] . " height='60' width='100'>} else <img src='../Slike/icon.png'>
-                            <br> Cena: " . $row["Price"] . " </article> </li>"; //za oceno še zrovn pa sliko po možnosti
+                            echo "Name: <a href='product.php?id=$id'>".$row['Title']."</a><br>"; if(isset($rowPicture['url'])){echo "<img src=".$rowPicture['url']." alt=".$rowPicture['Title']." height='60' width='100'>";} else{echo  "<img src='../Trgovko/Slike/icon.png'>";}
+                            echo "<br> Cena: " . $row["Price"] . " </article> </li>"; //za oceno še zrovn pa sliko po možnosti
                             ?>
                         </h6>
                     </div>
